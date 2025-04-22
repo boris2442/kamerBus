572c6c1900cc34b54e9e31ad46ab2c52cd60a599
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-22 21:37:50 +0200

    text

--- includes/header.php
+++ includes/header.php
@@ -16,7 +16,7 @@
     <header class="flex items-center justify-between p-4 md:px-10 shadow-sm bg-white dark:bg-[#121212]">
         <!-- Logo -->
         <div class="flex-shrink-0">
-            <img src="publics\assets\kamerbuslogojpg.jpg" alt="KamerBus Logo" class="h-10 w-auto" />
+            <img src="publics\assets\kamerbuslogojpg.jpg" alt="KamerBus Logo" class=" w-[70px] h-[70px] md:h-[100px] md:w-[100px] rounded-[50%]" />
         </div>
 
         <!-- Dark Mode Toggle -->
@@ -39,10 +39,12 @@
         <!-- Fullscreen Menu (Mobile) -->
         <div id="mobileMenu" class="fixed inset-0 bg-white dark:bg-[#121212] z-50 hidden flex-col items-center justify-center space-y-8 text-2xl font-semibold">
             <!-- Close Button -->
-            <button id="closeMenu" class="absolute top-5 right-5 text-4xl">&times;</button>
-            <a href="index.php" class="nav-link">Home</a>
-            <a href="about.php" class="nav-link">About</a>
-            <a href="contact.php" class="nav-link">Reservez</a>
+            <button id="closeMenu" class="absolute top-5 right-5 text-2xl">&times;</button>
+            <div class="flex flex-col gap-[10px]">
+                <a href="index.php" class="nav-link text-[15px]">Home</a>
+                <a href="about.php" class="nav-link text-[15px]">About</a>
+                <a href="contact.php" class="nav-link text-[15px]">Reservez</a>
+            </div>
         </div>
     </header>
 
