2f5868a4b5d0f54c7929f915f494b334a2dd7c50
boris2442 <aubinborissimotsebo@gmail.com>
2025-04-23 09:48:14 +0200

    text

--- index.php
+++ index.php
@@ -5,11 +5,11 @@ require_once "includes/header.php";
 <section class=" text-[#121212] dark:text-[#f1f1f1] py-12 px-6 md:px-16 lg:flex items-center justify-between">
   <!-- Texte -->
   <div class="max-w-xl mb-8 lg:mb-0">
-    <h1 class="text-3xl sm:text-4xl md:text-5xl font-bold mb-6">
+    <h1 class="text-3xl sm:text-4xl md:text-5xl font-bold mb-6 uppercase">
       Réservez votre voyage en toute simplicité avec KamerBUS
     </h1>
     <p class="text-base sm:text-lg md:text-xl leading-relaxed mb-6">
-      KamerBUS vous permet de réserver vos billets de bus en ligne en quelques clics. Évitez les files d’attente, comparez les horaires et voyagez sereinement partout au Cameroun.
+      KAMERBUS vous permet de réserver vos billets de bus en ligne en quelques clics. Évitez les files d’attente, comparez les horaires et voyagez sereinement partout au Cameroun.
     </p>
     <a href="#reservez" class="inline-block bg-[#FF7043] text-white px-6 py-3 rounded-xl text-sm sm:text-base font-medium transition duration-300 hover:bg-[#e45a2b]">
       Réservez maintenant
